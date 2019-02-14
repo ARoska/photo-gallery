412d44b2f2b150ef9a38d5edd42ef464ccb43c54
Andrew Roska <andrew.l.roska@gmail.com>
2019-02-14 10:56:56 -0800

    Completes render function

--- js/app.js
+++ js/app.js
@@ -11,6 +11,28 @@ function Animal(animal) {
 
 Animal.allAnimals = [];
 
+Animal.prototype.render = function() {
+  $('main').append('<div class="clone"></div>');
+  let animalClone = $('div[class="clone"]');
+
+  let animalHml = $('#photo-template').html();
+
+  animalClone.html(animalHml)
+
+  animalClone.find('h2').text(this.title);
+  animalClone.find('img').attr({
+    src: this.image_url,
+    title: this.title,
+    alt: this.description,
+  });
+  animalClone.find('p').text(this.description);
+  animalClone.append('<h3></h3>');
+  animalClone.find('h3').text(`Number of horns: ${this.horns}`);
+  animalClone.removeClass('clone');
+  animalClone.attr('class', this.title);
+  animalClone.attr('class', this.keyword);
+}
+
 Animal.readJson = () => {
   $.get('../data/page-1.json', 'json')
     .then(data => {
@@ -18,6 +40,9 @@ Animal.readJson = () => {
         Animal.allAnimals.push(new Animal(obj));
       })
     })
+    .then(Animal.loadAnimals)
 }
+
+Animal.loadAnimals = () => Animal.allAnimals.forEach(animal => animal.render())
 
 $(() => Animal.readJson());
